a1f2a0a3ea1915f1a3e24b0bfded977e43a75d21
MarkiianStefura <markiianstefura@gmail.com>
2019-08-26 16:37:20 +0300

    fix HW4

--- HomeWork4/src/ua/com/softserve/homework4/Example3.java
+++ HomeWork4/src/ua/com/softserve/homework4/Example3.java
@@ -10,36 +10,38 @@ public class Example3 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int[] arrayNumbers = new int[5];
-        int positionOfNumber= 0;
-        int min = Integer.MAX_VALUE;
-        int secondPositive = -1;
-
         for (int i = 0; i < arrayNumbers.length; i++) {
             System.out.println("Input " + (i + 1) + " number");
             arrayNumbers[i] = Integer.parseInt(br.readLine());
-            if(arrayNumbers[i]<min){
+        }
+
+        int positionOfNumber = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arrayNumbers.length; i++) {
+            if (arrayNumbers[i] < min) {
                 min = arrayNumbers[i];
                 positionOfNumber = i;
             }
         }
         System.out.println("Min = " + min + " it is position = " + positionOfNumber);
 
-        boolean secondPositivefound = false;
-        int[] array = new int[] {-2, 3, 3, -5, 8};
-        for (int i = 0; i < array.length; i++) {
-            if(array[i] > 0) {
-                for (int s = i + 1; s < array.length ; s++ ) {  //  Вкрай нераціонально
-                    if (array[s] > 0) {
-                        secondPositive = s;
-                        secondPositivefound = true;
-                        break;
-                    }
-                }
-            }
-            if (secondPositivefound) {
+        int secondPositivePosition = 0;
+        boolean firstPositiveFound = false;
+        boolean secondPositiveFound = false;
+
+        for (int i = 0; i < arrayNumbers.length; i++) {
+            if (arrayNumbers[i] > 0 && firstPositiveFound) {
+                secondPositivePosition = i;
+                secondPositiveFound = true;
                 break;
             }
+            if (arrayNumbers[i] > 0) {
+                firstPositiveFound = true;
+            }
         }
-        System.out.println("Second positive number is  " + secondPositive);
+
+        if (secondPositiveFound) {
+            System.out.println("Second positive number is  " + secondPositivePosition);
+        }
     }
 }
