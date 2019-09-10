2f9556721bf41be21890b587563175adaad9be61
Muzyka Oleksandr <myzzon93@gmail.com>
2019-09-10 17:34:47 +0300

    9 task

---
+++ Tasks/src/com/company/Task9.java
@@ -0,0 +1,16 @@
+public class Task_9 {
+
+
+    public void findOne(int n) {
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int number = (int) Math.round(Math.random());
+            
+            if (number == 1) {
+                count++;
+            }
+
+
+        }
+        System.out.println("We have 1 for " +count+ " times");
+    }
