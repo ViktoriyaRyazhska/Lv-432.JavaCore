a7508a6bbdbe6dfadb578da9d5536123ea164354
tolstyy113 <tolstyy11341@gmail.com>
2019-09-10 17:39:56 +0300

    Task 5 6

---
+++ Tasks/src/com/company/Task5.java
@@ -0,0 +1,12 @@
+package TaskForMy;
+/**
+ * 5.	Input string and check if this string is palindrome (e.g. “ABCCBA”)
+ * @author 999
+ *
+ */
+
+public class Task5 {
+	public static boolean isPalindrome(String str) {
+	    return str.equals(new StringBuilder(str).reverse().toString());
+	}
+}
---
+++ Tasks/src/com/company/Task6.java
@@ -0,0 +1,23 @@
+package TaskForMy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Input text and calculate number of words in this text
+ * @author 999
+ *
+ */
+public class Task6 {	
+	public static int numberOfWords(String text) {
+		List<String> textOfWord = new ArrayList<String>();	
+		Pattern pattern = Pattern.compile("[A-Za-z]+");
+		Matcher matcher = pattern.matcher(text);
+		while (matcher.find()) {			
+			textOfWord.add(text.substring(matcher.start(), matcher.end()));
+		}
+		return textOfWord.size();		
+	}
+}
